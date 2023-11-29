e4719e33a9fd216100d9129700df6232ac03dbc6
andrew-suh <andrew.suh.94@gmail.com>
2023-11-28 19:04:01 -0800

    Created fleet template for ships

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -5,6 +5,9 @@ export const Settings = Object.freeze({
     dimension: 6,
     nodeSize: 100,
     nodeOffset: 10,
-    color: new Color(245,245,245,1)
+    color: new Color(245,245,245,1),
+  },
+  ships: {
+    fleetSize: 3
   }
 })
